8f45ffc4e48de8b1fc17a8b69a3c9c5a85a6950f
BruceXu <xudage2011@gmail.com>
2026-08-19 01:35:48 +0800

    Add backend CI test workflow

---
+++ SpringBootBackend/src/test/java/com/brucexu/springBootBackend/DummyTests.java
@@ -0,0 +1,15 @@
+package com.brucexu.springBootBackend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DummyTest {
+
+    @Test
+    void githubActionsTest() {
+        int result = 2 + 2;
+
+        assertEquals(4, result);
+    }
+}
