eef32b1dbcc88cd93e0a2b69e6cc92bc8933f487
BruceXu <xudage2011@gmail.com>
2026-08-19 01:41:10 +0800

    minor fixes

--- SpringBootBackend/src/test/java/com/brucexu/springBootBackend/DummyTests.java
+++ SpringBootBackend/src/test/java/com/brucexu/springBootBackend/DummyTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DummyTest {
+class DummyTests {
 
     @Test
     void githubActionsTest() {
